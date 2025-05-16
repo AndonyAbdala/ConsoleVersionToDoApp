d355faf46898c3d383eb0c2794ee7a073440bcbc
GAA6GA <AndonyAbdala.GarciaGarcia@mx.bosch.com>
2025-05-15 23:31:26 -0600

    New feature "complete"

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
-# message = "Enter a to do: "
-# todo1 = input(message)
-# todo2 = input(message)
-# todo3 = input(message)
-
-# todos = [todo1, todo2, todo3]
-# print(todos)
-# print(type(todos))
-# print(type(todo1))
-
-# text = input("Enter something: ")
-# print("Number of letters is: " + str(len(text)))
-
 todo_list = []
 
 while True: 
-    user_prompt = input("Type add, edit, show or exit: ")
+    user_prompt = input("Type add, edit, show, complete or exit: ")
     user_prompt = user_prompt.strip()
     match user_prompt:
         case "add":
             todo_item = input("Enter a todo: ")
             todo_list.append(todo_item)
         case "show":
-            for todo_element in todo_list:
-                print(todo_element)
+            for index, todo_element in enumerate(todo_list):
+                print(f"{index + 1}.- {todo_element.capitalize()}")
         case "edit":
             item_index = int(input("Enter the number of item you want to modify: "))
             item_index = item_index - 1
             value = input("Enter the new todo: ")
             todo_list[item_index] = value
+        case "complete":
+            item_index = int(input("Enter the number of item you have completed: "))
+            todo_list.pop(item_index - 1)
+            print("Pending activities: ")
+            for index, todo_element in enumerate(todo_list):
+                print(f"{index + 1}.- {todo_element}")
         case "exit":
             print("Good bye!")
             break
